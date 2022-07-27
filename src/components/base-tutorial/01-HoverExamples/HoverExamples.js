db88e9b5bb129f7fd63b9ce76b919b5b0cf0fb07
FOD17 <w_mark81@yahoo.com>
2022-07-27 18:53:04 -0500

    changes

---
+++ src/components/base-tutorial/01-HoverExamples/HoverExamples.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HoverExamples() {
+  return (
+    <div>
+      <button class="bg-sky-600 hover:bg-sky-700 ...">Save changes</button>
+    </div>
+  );
+}
