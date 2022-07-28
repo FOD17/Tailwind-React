53cba5d477c92c68c887da64ef78f47ea80d6fa1
Mark Williams <markwilliams@marks-mbp-2.lan>
2022-07-27 19:58:19 -0500

    mo stuff

--- src/components/base-tutorial/01-HoverExamples/HoverExamples.js
+++ src/components/base-tutorial/01-HoverExamples/HoverExamples.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react"
 
+// Can't chain multiple hover/focus examples. One at a time
 export default function HoverExamples() {
   return (
     <div>
-      <button class="bg-sky-600 hover:bg-sky-700 ...">Save changes</button>
+      <button className="bg-sky-600 hover:bg-sky-700 active:text-7xl font-extrabold text-4xl rounded p-4">
+        Save changes
+      </button>
+      <div className="mt-6">
+        <label htmlFor="example">Name:</label>
+        <input
+          type="text"
+          id="example"
+          className="focus:bg-red-600 border-solid border-black border-2"
+        />
+      </div>
     </div>
-  );
+  )
 }
